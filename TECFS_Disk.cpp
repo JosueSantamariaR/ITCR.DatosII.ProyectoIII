5d1b9c95762ec81594deb12e33bec34d0f9bcf37
Josue Santamaria <48070852+Santamix728@users.noreply.github.com>
2021-06-24 23:55:40 -0600

    Doxygen

--- TECFS_Disk.cpp
+++ TECFS_Disk.cpp
@@ -1,8 +1,16 @@
+/**
+  * @file TECFS_Disk.cpp
+  * */
 #include <dirent.h>
 #include "TECFS_Disk.h"
 
 int TECFS_Disk::diskNumber = 0;
 
+/**
+ * @brief Constructor
+ * @param string IP, int por, string path, int diskSize, int blockSize, bool isFirstDisk
+ * @authors Ricardo&Yordi&Josue
+ */
 TECFS_Disk::TECFS_Disk(string IP, int port, string path, int diskSize, int blockSize, bool isFirstDisk) {
     this->IP = IP;
     this->port = port;
@@ -20,7 +28,10 @@ TECFS_Disk::TECFS_Disk(string IP, int port, string path, int diskSize, int block
 
     initDisk();
 }
-
+/**
+ * @brief Metodo para cuando es el primer disco
+ * @authors Ricardo&Yordi&Josue
+ */
 void TECFS_Disk::initDisk() {
     string diskDirectory = diskPath + "/Disk";
     if (isFirstDisk){
@@ -34,23 +45,43 @@ void TECFS_Disk::initDisk() {
     diskDirectory = diskDirectory + "_" + to_string(diskNumber);
     filesystem::create_directory(diskDirectory);
 }
-
+/**
+ * @brief Metodo para obtener IP
+ * @return IP
+ * @authors Ricardo&Yordi&Josue
+ */
 const string &TECFS_Disk::getIp() const {
     return IP;
 }
-
+/**
+ * @brief Metodo para obtener diskPath
+ * @return diskPath
+ * @authors Ricardo&Yordi&Josue
+ */
 const string &TECFS_Disk::getDiskPath() const {
     return diskPath;
 }
-
+/**
+ * @brief Metodo para obtener port
+ * @return port
+ * @authors Ricardo&Yordi&Josue
+ */
 int TECFS_Disk::getPort() const {
     return port;
 }
-
+/**
+ * @brief Metodo para obtener diskSize
+ * @return diskSize
+ * @authors Ricardo&Yordi&Josue
+ */
 int TECFS_Disk::getDiskSize() const {
     return diskSize;
 }
-
+/**
+ * @brief Metodo para obtener blockSize
+ * @return blockSize
+ * @authors Ricardo&Yordi&Josue
+ */
 int TECFS_Disk::getBlockSize() const {
     return blockSize;
 }
